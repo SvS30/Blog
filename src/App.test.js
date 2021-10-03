db02e12a0f897c184ebfb7d7982956c1b138cd17
Salim Vazquez Solis <salimvzqz@gmail.com>
2021-10-02 20:32:23 -0500

    New test cases added for new components

--- src/App.test.js
+++ src/App.test.js
@@ -12,6 +12,22 @@ test('renders sidebar component in App', () => {
 
 test('renders about component in App', () => {
   render(<App />);
-  const aboutElement = screen.getByText(/this is a bootstrap v4.2.1 CSS Template/i);
+  const aboutElement = screen.getByText(/acerca de mi/i);
   expect(aboutElement).toBeInTheDocument();
+  const readMoreElement = screen.getAllByText(/read more/i);
+  expect(readMoreElement.length).toBe(2);
+});
+
+test('renders services component in App', () => {
+  render(<App />);
+  const titleServiceElement = screen.getByText(/what is lorem ipsum/i);
+  expect(titleServiceElement).toBeInTheDocument();
+  const titleCardOneElement = screen.queryAllByText(/where does it come from/i);
+  expect(titleCardOneElement.length).toBe(2);
+  const titleCardTwoElement = screen.getAllByText(/why do we use it/i);
+  expect(titleCardTwoElement.length).toBe(2);
+  const titleCardThreeElement = screen.getByText(/where can i get some/i);
+  expect(titleCardThreeElement).toBeInTheDocument();
+  const titleCardFourElement = screen.getByText(/the standard lorem ipsum/i);
+  expect(titleCardFourElement).toBeInTheDocument();
 });
