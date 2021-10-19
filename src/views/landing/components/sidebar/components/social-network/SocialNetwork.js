3ac57aaa3223efcbf622bef607c99ac5e4669c29
Salim Vazquez Solis <salimvzqz@gmail.com>
2021-10-19 15:21:23 -0500

    Social networks addeds

--- src/views/landing/components/sidebar/components/social-network/SocialNetwork.js
+++ src/views/landing/components/sidebar/components/social-network/SocialNetwork.js
@@ -5,19 +5,19 @@ function SocialNetwork() {
         <div className="social-network">
             <ul className="soial-icons">
                 <li>
-                    <a href="http://google.com"><i className="fa fa-facebook"></i></a>
+                    <a href="https://twitter.com/salim_vazquez"><i className="fa fa-twitter"></i></a>
                 </li>
                 <li>
-                    <a href="http://google.com"><i className="fa fa-twitter"></i></a>
+                    <a href="https://github.com/SvS30"><i className="fa fa-github"></i></a>
                 </li>
                 <li>
-                    <a href="http://google.com"><i className="fa fa-linkedin"></i></a>
+                    <a href="https://gitlab.com/SvS21"><i class="fa fa-gitlab"></i></a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/salim-vazquez-solis-6565b7170/"><i className="fa fa-linkedin"></i></a>
                 </li>
                 <li>
                     <a href="http://google.com"><i className="fa fa-dribbble"></i></a>
-                </li>
-                <li>
-                    <a href="http://google.com"><i className="fa fa-rss"></i></a>
                 </li>
             </ul>
         </div>
