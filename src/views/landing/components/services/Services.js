698bcb4c1f84d0852191e819cddf7ef32365b5dd
Salim Vazquez Solis <salimvzqz@gmail.com>
2021-10-24 12:52:48 -0500

    Skills updated

--- src/views/landing/components/services/Services.js
+++ src/views/landing/components/services/Services.js
@@ -7,14 +7,14 @@ import ServiceItem from './components/service-item/ServiceItem';
 
 class Services extends Component {
     state = {
-        cardOne: ["first-service-icon", "Capacidad de análisis e investigación", `Contrary to popular belief, Lorem Ipsum is not simply random text.
-            It has roots in a piece of classical Latin literature from 45 BC.`],
-        cardTwo: ["second-service-icon", "Capacidad para tomar decisiones y resolver problemas", `It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout.`],
-        cardThree: ["third-service-icon", "Capacidad de adaptarse a nuevos entornos de trabajo", `There are many variations of passages of Lorem Ipsum available,
-            but the majority have suffered alteration in some form.`],
-        cardFour: ["fourth-service-icon", "Capacidad de organización y planificación", `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-            do eiusmod tempor incididunt ut labore et dolore magna aliqua.`]
+        cardOne: ["first-service-icon", "Capacidad de análisis e investigación", `Como desarrollador de software es de suma importancia mantenerse actualizado
+        y a la vanguardia, por lo que he estado puliendo el ser autodidacta, y poner la teoría en práctica.`],
+        cardTwo: ["second-service-icon", "Capacidad para tomar decisiones y resolver problemas", `Al ser los problemas inevitables en el día a día, he conseguido
+        mejorar el identificar el origen de la problemática, analizar las posibles soluciones e implementar la mejor solución.`],
+        cardThree: ["third-service-icon", "Capacidad de adaptarse a nuevos entornos de trabajo", `Durante el desarrollo de mis estudios universitarios
+        desarrollé la suficiente flexibilidad a los cambios que ocurran en mi entorno y saber adaptarme con facilidad a la nuevas realidades que se estan presentando.`],
+        cardFour: ["fourth-service-icon", "Capacidad de organización y planificación", `En mi formacion como ingeniero adquiri la capacidad para desarrollar
+        la aptitud mas adecuada para programar y distribuir las tareas entre el equipo de trabajo, estableciendo objetivos realistas.`]
     };
 
     render() {
