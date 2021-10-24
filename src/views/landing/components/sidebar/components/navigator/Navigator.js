a95e8e531cfd63713d1d276ba2959da5dc5c2051
Salim Vazquez Solis <salimvzqz@gmail.com>
2021-10-24 12:51:52 -0500

    Fix DOM in SocialNetwork and CV link added in nav

--- src/views/landing/components/sidebar/components/navigator/Navigator.js
+++ src/views/landing/components/sidebar/components/navigator/Navigator.js
@@ -6,6 +6,8 @@ function Navigator() {
             <ul className="main-menu">
                 <li><a href="#section1">Acerca de Mi</a></li>
                 <li><a href="#section2">Aptitudes y Habilidades</a></li>
+                <li><a href="https://drive.google.com/file/d/1E92zSO-y81Uz33JiqlIrFC69A4ELx4dd/view?usp=sharing"
+                    target="_blank" rel="noopener noreferrer">CV</a></li>
             </ul>
         </nav>
     );
